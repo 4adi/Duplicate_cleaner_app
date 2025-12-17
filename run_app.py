e40a306126b49ac778a69b51d8a83e17db8ee8ba
Aadil Anvar <aadila@inerg.com>
2025-12-17 03:19:38 -0600

    Build exe with icon, versioning and optimizations

--- run_app.py
+++ run_app.py
@@ -1,31 +1,19 @@
 import subprocess
 import sys
 import os
-import time
-import webbrowser
 
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     app_path = os.path.join(base_dir, "duplicate_cleaner_app.py")
 
-    process = subprocess.Popen([
+    subprocess.run([
         sys.executable,
         "-m",
         "streamlit",
         "run",
         app_path,
-        "--server.headless=true",
-        "--browser.gatherUsageStats=false",
-        "--server.port=8501"
+        "--browser.gatherUsageStats=false"
     ])
-
-    # Give Streamlit time to start
-    time.sleep(3)
-
-    # Auto-open browser
-    webbrowser.open("http://localhost:8501")
-
-    process.wait()
 
 if __name__ == "__main__":
     main()
